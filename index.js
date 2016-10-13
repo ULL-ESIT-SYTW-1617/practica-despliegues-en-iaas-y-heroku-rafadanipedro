e0bd18d6dd266706e07e567f964897fbf7a2e3ef
alu0100505078 <alu0100505078@ull.edu.es>
2016-10-13 16:55:02 +0000

    finalizado index.js hace pull,si no clona y llama al script y crea gitbook

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var logger = require('morgan');
+'use strict'
 
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const Git = require('simple-git')
+const fs = require('fs')
+const spawn = require('child_process').spawn
 
-const port = process.env.PORT || 8080;
+
+const config = require('./package.json')
+
+let git
+if (fs.existsSync('./gitbook')) {
+  git = Git(__dirname + '/gitbook')
+} else {
+  git = Git(__dirname)
+}
+
+let app = express();
 
 app.use(logger('dev'));
 
@@ -14,15 +27,57 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.get('*', function (req, res) {
-  console.log(req.body);
-  res.send('Hello World!');
-});
+app.use(express.static('gitbook/_book'));
 
 app.post('*', function (req, res) {
-  console.log(req.body);
-  res.send('Hello World!');
+  if (/GitHub-Hookshot/.test(req.headers['user-agent'])) {
+    if (fs.existsSync('./gitbook')) {
+      console.log('Ya existe gitbook, sólo pulleo')
+      pull().then(() => {
+        generarGitbook().then(data => {
+          res.send('Gitbook correctamente generado!');
+        }).catch(err => {
+          res.status(500).send('Error!!');
+        })
+        console.log('Pulleado correctamente!')
+      });
+    } else {
+      console.log('No existe el gitbook, así que clono')
+      clonar(req.body.repository.clone_url).then(() =>{
+        git.cwd('./gitbook')
+        generarGitbook().then(data => {
+          res.send('Gitbook correctamente generado!');
+        }).catch(err => {
+          res.status(500).send('Error!!');
+        })
+        console.log('Clonado correctamente!')
+      })
+    }
+
+  } else {
+    res.status(403).send('Sólo puedes regenerar el gitbbok desde el webhook de github. Así que sólo pushea.')
+  }
 });
+
+// Revisar si existe el directorio gitbook, si no crearlo.
+function clonar(url) {
+  return git.clone(url, __dirname + '/gitbook')
+}
+function pull(){
+  return git.pull()
+}
+
+function generarGitbook () {
+  return new Promise((resolve, reject) => {
+    const gb = spawn('bash', ['generarGitbook.sh'])
+
+    gb.stdout.on('data', data => process.stdout.write(data.toString()))
+    gb.stderr.on('data', data => process.stderr.write(data.toString()))
+    gb.on('close', code => code ? reject() : resolve())
+  })
+}
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
